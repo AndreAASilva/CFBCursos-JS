5c15c4646c582572e29c4289df6c21607c582bb4
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2023-05-11 16:41:15 -0300

    Fim da aula 16

---
+++ CFB_Cursos/aula015.js
@@ -0,0 +1,24 @@
+/* Loop FOR em Javascript */
+
+/*Loops são classificados como definidos (quando há definida a quantidade de comandos) e indefinidos (quando não há definição da quantidade de comandos)*/
+
+
+for(let i = 0; i <= 20; i+=2){
+    console.log('André Anderson - Valor do i: '+ i)
+}
+
+console.log('FIM DO PROGRMA')
+
+/*
+console.log('INÍCIO DO PROGRAMA')
+
+for(let i = 0; i <= 20; i++){
+    if(i%2==0){
+        console.log(i + ' é par')
+    }else{
+        console.log(i+' é ímpar')
+    }
+}
+
+console.log('FIM DO PROGRAMA')
+*/
---
+++ CFB_Cursos/aula016.js
@@ -0,0 +1,68 @@
+/* Loops FOR IN e FOR OF em Javascript */
+
+/*Variações do FOR*/
+
+/*
+let num = [10,20,30,40,50]
+
+//Aqui ele vai me informar as posições dos elementos
+for(let i = 0; i < num.length; i++){
+    console.log(i)
+}
+
+//Aqui ele vai informar os valores do elementos
+for(let i = 0; i < num.length; i++){
+    console.log(num[i])
+}
+
+//Com o FOR IN desta forma ele vai me informar as posições dos elementos
+for(n in num){
+    console.log(n)
+}
+
+//Com o FOR IN desta forma ele vai me informar os valores dos elementos
+for(n in num){
+    console.log(num[n])
+}
+
+//Com o FOR OF desta forma ele vai me informar os valores dos elementos
+for(n of num){
+    console.log(n)
+}
+*/
+
+/*
+const objs = document.getElementsByTagName('div')
+
+for(o of objs){
+    console.log(o.innerHTML='Aula de FOR IN e FOR OF')
+}
+
+for(o in objs){
+    console.log(objs[o].innerHTML)
+}
+
+for(o in objs){
+    console.log(o)
+}
+*/
+const nome = ['André', 'Anderson', 'Alves', 'Silva']
+const nome01 = nome['Alves']
+
+/*
+for(i in nome){
+    console.log(nome[i])
+}
+
+for(i of nome){
+    console.log(i)
+}
+*/
+
+if(nome){
+    for(i of nome){
+        if(i == 'Alves'){
+            console.log('Nome localizado')
+        }
+    }
+}
