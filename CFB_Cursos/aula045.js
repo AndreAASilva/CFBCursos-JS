78204d15a00422a99e1b24f7fbe7aae2894f893a
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2023-05-25 21:38:52 -0300

    Fim da aula 47

---
+++ CFB_Cursos/aula045.js
@@ -0,0 +1,116 @@
+//Criando elementos #P2 antes e depois de outros elementos
+
+const caixaCursos = document.querySelector('#caixaCursos')
+const btn_c = document.querySelectorAll('.curso')
+const c1_2 = document.querySelector('#c1_2')
+const cursos = ['HTML', 'CSS', 'Javascript', 'PHP', 'React', 'MySQL', 'ReactNative']
+const btnCursoSelecionado=document.getElementById('btnCursoSelecionado')
+const btnRemoverCurso = document.getElementById('btnRemoverCurso')
+const btnAdicionarNovoCursoAntes = document.getElementById('btnAdicionarNovoCursoAntes')
+const btnAdicionarNovoCursoDepois = document.getElementById('btnAdicionarNovoCursoDepois')
+const nomeCurso = document.getElementById('nomeCurso')
+
+let indice = 0
+
+
+const criarNovoCurso=(curso)=>{
+    const novoElemento=document.createElement('div')
+    novoElemento.setAttribute('id', 'c'+indice)
+    novoElemento.setAttribute('class','curso c1')
+    novoElemento.innerHTML=curso
+    
+    const comandos = document.createElement('div')
+    comandos.setAttribute('class', 'comandos')
+
+    const rb = document.createElement('input')
+    rb.setAttribute('type', 'radio')
+    rb.setAttribute('name', 'rb_curso')
+
+    comandos.appendChild(rb)
+
+    novoElemento.appendChild(comandos)
+    return novoElemento
+}
+
+
+cursos.map((el,chave)=>{
+    const novoElemento=criarNovoCurso(el)
+    caixaCursos.appendChild(novoElemento)
+    indice++
+})
+
+//Removendo os elementos
+
+const radioSelecionado=()=>{
+    const todosRadios=[...document.querySelectorAll('input[type=radio]')]
+    const radioSelecionado=todosRadios.filter((ele, ind, arr)=>{
+        return ele.checked
+    })
+
+    return radioSelecionado[0]
+}
+
+
+//Selecionando os elementos e com try e catch(gerar exceção)
+btnCursoSelecionado.addEventListener('click', (evt)=>{
+    const rs = radioSelecionado()
+    try{
+        const cursoSelecionado = rs.parentNode.previousSibling.textContent
+        alert('Curso selecionado: '+ cursoSelecionado)
+    }catch(ex){
+        alert('Selecione um curso')
+    }
+    
+    
+})
+
+//Utilizando o remove() para remover os elementos selecionados
+btnRemoverCurso.addEventListener('click',(evt)=>{
+    const rs = radioSelecionado()
+    if(rs!=undefined){
+        const cursoSelecionado = rs.parentNode.parentNode
+        cursoSelecionado.remove()
+    }else{
+        alert('Selecione um curso')
+    }
+    
+})
+
+//Criados eventos para o botões onde serão adicionados novos cursos e utilizando funções acima criadas nas aulas anteriores junto com constantes tanto novas como já criadas em aulas anteriores também.
+
+btnAdicionarNovoCursoAntes.addEventListener('click', (evt)=>{
+    const rs = radioSelecionado()
+    try{
+        if(nomeCurso.value!=''){
+            const cursoSelecionado = rs.parentNode.parentNode
+            const novoCurso=criarNovoCurso(nomeCurso.value)
+            caixaCursos.insertBefore(novoCurso,cursoSelecionado) //Método insertBefore cirado para inserir o curso antes do clicado no programa, dentro dos parâmetros primeiro inserimos a constante com o nome do curso novo e depois a constante selecionada para ele identificar onde o curso novo será posicionado 
+        }else{
+            alert('Digite o nome do curso')
+        }
+    }catch(ex){
+        alert('Selecione um curso')
+    }
+})
+
+btnAdicionarNovoCursoDepois.addEventListener('click', (evt)=>{
+    const rs = radioSelecionado()
+    try{
+        if(nomeCurso.value!=''){
+            const cursoSelecionado = rs.parentNode.parentNode
+            const novoCurso=criarNovoCurso(nomeCurso.value)
+            caixaCursos.insertBefore(novoCurso,cursoSelecionado.nextSibling) //Como não há o método para inserir um curso depois, apenas antes com o insertBefore(), utilizamos a lógica de chamar um irmão da classe nesse caso, a frente de onde será clicado para ao inserir, o código posicione o curso novo antes do irmão.
+        }else{
+            alert('Digite o nome do curso')
+        }
+    }catch(ex){
+        alert('Selecione um curso')
+    }
+})
+
+//parentNode
+//childNodes[nodenumber]
+//firstChild
+//lastChild
+//nextSibling
+//previousSibling
---
+++ CFB_Cursos/aula047.js
@@ -0,0 +1,23 @@
+//Método FIND para encontrar elementos em Arrays em Javascript 
+
+const p_array = document.querySelector('#array')
+const txt_pesquisar = document.querySelector('#txt_pesquisar')
+const btnPesquisar = document.querySelector('#btnPesquisar')
+const resultado = document.querySelector('#resultado')
+
+const elementos_array = [10,5,8,2,9,15,20]
+p_array.innerHTML = '['+elementos_array+']'
+
+btnPesquisar.addEventListener('click',(evt)=>{
+    const ret = elementos_array.find((e,i)=>{
+        if(e.toUpperCase()==txt_pesquisar.value.toUpperCase()){
+            resultado.innerHTML= 'O resultado pesquisaro é: ' + e + ' na posição: '+ i
+            return e
+        }else{
+            resultado.innerHTML= 'Valor não relacionado'
+        }
+    })
+
+    
+    console.log(ret)
+})
