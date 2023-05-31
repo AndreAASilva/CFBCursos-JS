05be6958b1098687f953a8bfa62ab7045c7dcfaa
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2023-05-31 15:37:03 -0300

    Calculadora Aula053
    Finalizada calculadora simples da aula 053

--- CFB_Cursos/aula052_calc.js
+++ CFB_Cursos/aula052_calc.js
@@ -1,15 +1,33 @@
-let valor01 = document.getElementById('valor01')
-let valor02 = document.querySelector('#valor02')
-let resultado = document.querySelector('#resultado')
-
 const btnAdicao = document.querySelector('#adicao')
 const btnSubtracao = document.querySelector('#subtracao')
 const btnMultiplizacao = document.querySelector('#multiplicacao')
 const btnDivisao = document.querySelector('#divisao')
 
 
+const primeiroValor = document.getElementById('valor01')
+const segundoValor = document.getElementById('valor02')
+const resultado = document.getElementById('resultado')
+
 btnAdicao.addEventListener('click',()=>{
-   
+    const val1 = Number(primeiroValor.value)
+    const val2 = Number(segundoValor.value)
+    resultado.value = val1 + val2
 })
 
+btnSubtracao.addEventListener('click',()=>{
+    const val1 = Number(primeiroValor.value)
+    const val2 = Number(segundoValor.value)
+    resultado.value = val1 - val2
+})
 
+btnMultiplizacao.addEventListener('click',()=>{
+    const val1 = Number(primeiroValor.value)
+    const val2 = Number(segundoValor.value)
+    resultado.value = val1 * val2
+})
+
+btnDivisao.addEventListener('click',()=>{
+    const val1 = Number(primeiroValor.value)
+    const val2 = Number(segundoValor.value)
+    resultado.value = val1 / val2
+})
